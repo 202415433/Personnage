cea09c16584561f71c3695dbe3920a1a03bbfd03
karambaGassama@11 <karagascanada@gmail.com>
2025-06-16 13:28:20 -0400

    creation du fichier de recuperation de charater-details

---
+++ src/app/components/character-details/character-details.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Character } from '../../models/character.interface';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.scss']
+})
+export class CharacterDetailsComponent {
+  characterId: number | null = null;
+  character: Character | null = null;
+  loading = false;
+  error: string | null = null;
+
+  constructor(private rickAndMortyService: RickAndMortyService) { }
+
+  searchCharacter(): void {
+    if (!this.characterId || this.characterId <= 0) {
+      this.error = 'Veuillez entrer un ID valide (nombre positif)';
+      return;
+    }
+
+    this.loading = true;
+    this.error = null;
+    this.character = null;
+
+    this.rickAndMortyService.getCharacterById(this.characterId)
+      .subscribe({
+        next: (character) => {
+          this.character = character;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = 'Personnage non trouvé ou erreur de chargement';
+          this.loading = false;
+        }
+      });
+  }
+}
