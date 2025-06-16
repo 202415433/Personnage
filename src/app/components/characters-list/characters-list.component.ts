b9fc028ff259495109dc369872931c054bac0708
karambaGassama@11 <karagascanada@gmail.com>
2025-06-16 12:40:25 -0400

    Creation du fichier characters-list pour la recuperation des personnage

---
+++ src/app/components/characters-list/characters-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Character } from '../../models/character.interface';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-characters-list',
+  templateUrl: './characters-list.component.html',
+  styleUrls: ['./characters-list.component.scss']
+})
+export class CharactersListComponent implements OnInit {
+  characters: Character[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private rickAndMortyService: RickAndMortyService) { }
+
+  ngOnInit(): void {
+    this.loadCharacters();
+  }
+
+  loadCharacters(): void {
+    this.loading = true;
+    this.error = null;
+    
+    this.rickAndMortyService.getAllCharacters()
+      .subscribe({
+        next: (response) => {
+          this.characters = response.results;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = 'Erreur lors du chargement des personnages';
+          this.loading = false;
+        }
+      });
+  }
+}
